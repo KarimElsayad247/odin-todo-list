4eca938e6051785977f2957301de17fdcb1b8c37
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-11-05 16:57:38 +0200

    Add a button to the sidebar

--- src/modules/Sidebar.js
+++ src/modules/Sidebar.js
@@ -1,5 +1,27 @@
+const CreateProjectButton = () => {
+    const button = document.createElement('button');
+    button.textContent = "+"
+    button.id = "add-project-button";
+    return button;
+}
+
 export default function() {
     let sidebar = document.createElement('section');
     sidebar.classList.add('sidebar');
+ 
+    const sidebarHeader = document.createElement('div');
+    sidebarHeader.id = 'sidebar-header'
+
+    const sidebarHeaderTitle = document.createElement('h2');
+    sidebarHeaderTitle.textContent = "Projects";
+    sidebarHeaderTitle.classList.add("sidebar-header-title")
+ 
+    const createProjectButton = CreateProjectButton();
+
+    sidebarHeader.appendChild(sidebarHeaderTitle);
+    sidebarHeader.appendChild(createProjectButton);
+    
+    sidebar.appendChild(sidebarHeader);
+
     return sidebar;
 }
